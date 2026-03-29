c79fd36f21985788b76f25453e14cf2073ed1bb3
Dhanraj146 <bogadhanraj51@gmail.com>
2026-03-29 12:55:27 +0530

    add new feature multiply

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ function subtract(a,b){
     if(!(a>b)) throw new error("num a must be greater than b");
     return a-b;
 }
+function multiply(a,b){
+    return a*b;
+}
+
 add(10,20);
 add(30,40)
 add(50,60)
