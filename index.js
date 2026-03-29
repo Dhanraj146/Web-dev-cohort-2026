742cbca55848ad5f1fcd1763a9463f9bb9c281cc
Dhanraj146 <bogadhanraj51@gmail.com>
2026-03-29 12:37:04 +0530

    merged the feature/a into main

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ function add(a,b){
     return a+b;
 }
 
+function subtract(a,b){
+    if(!(a>b)) throw new error("num a must be greater than b");
+    return a-b;
+}
 add(10,20);
 add(30,40)
 add(50,60)
+
+subtract(10,20);
