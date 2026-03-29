7cccecbad5586a9589ad5baf5d857e396d43d51a
Dhanraj146 <165544014+Dhanraj146@users.noreply.github.com>
2026-03-29 13:24:18 +0530

    Feature/multiply (#1)
    * add new feature multiply
    
    * add function call to multiply for working
    
    * add another function call to check
    
    * add another function call to again check working
    
    * another function call to just test the last commit after push
    
    * added validation condition

--- index.js
+++ index.js
@@ -6,8 +6,18 @@ function subtract(a,b){
     if(!(a>b)) throw new error("num a must be greater than b");
     return a-b;
 }
+function multiply(a,b){
+    if(a<0 || b<0) throw error("one num is negative");
+    return a*b;
+}
+
 add(10,20);
 add(30,40)
 add(50,60)
 
 subtract(10,20);
+
+multiply(5,2);
+multiply(10,2);
+multiply(20,5);
+multiply(50,1);
